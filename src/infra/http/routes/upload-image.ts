c35bbc803a2d1d648b9c2ae4bee2180d5ef1d009
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2026-03-22 13:13:00 -0300

    feat: new id system implemented

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,3 +1,5 @@
+import { db } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import z from 'zod'
 
@@ -20,6 +22,11 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
       },
     },
     async (_request, reply) => {
+      await db.insert(schema.uploads).values({
+        name: 'teste2.jpg',
+        remoteKey: 'teste2.jpg',
+        remoteUrl: 'http://teste2.com',
+      })
       return reply.status(201).send({ uploadId: 'teste' })
     },
   )
